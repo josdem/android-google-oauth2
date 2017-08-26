4e8e661292591f3bedc36f926e1db8bbfc9ac417
josdem <joseluis.delacruz@gmail.com>
2017-08-26 17:24:01 -0500

    Adding status connection as text view

--- app/src/main/java/com/jos/dem/oauth2/MainActivity.java
+++ app/src/main/java/com/jos/dem/oauth2/MainActivity.java
@@ -3,29 +3,44 @@ package com.jos.dem.oauth2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
   private static final int RC_SIGN_IN = 9001;
+  private static final String TAG = "Oauth2 by Google";
   private GoogleApiClient mGoogleApiClient;
+  private TextView statusTextView;
+  private SignInButton signInButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    statusTextView = (TextView) findViewById(R.id.status);
+
     GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .build();
 
-    SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+    mGoogleApiClient = new GoogleApiClient.Builder(this)
+            .enableAutoManage(this, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+            .build();
+
+    signInButton = (SignInButton) findViewById(R.id.sign_in_button);
     signInButton.setOnClickListener(this);
   }
 
@@ -37,6 +52,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
   private void signIn() {
     Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
     startActivityForResult(signInIntent, RC_SIGN_IN);
+  }
+
+  @Override
+  public void onConnectionFailed(ConnectionResult connectionResult) {
+    Log.d(TAG, "onConnectionFailed:" + connectionResult);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == RC_SIGN_IN) {
+      GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+      handleSignInResult(result);
+    }
+  }
+
+  private void handleSignInResult(GoogleSignInResult result) {
+    Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+    if (result.isSuccess()) {
+      GoogleSignInAccount account = result.getSignInAccount();
+      statusTextView.setText("Signed as: " + account.getEmail());
+      signInButton.setVisibility(View.GONE);
+    } else {
+      statusTextView.setText("Singed in unsuccessfully");
+    }
   }
 
 }
