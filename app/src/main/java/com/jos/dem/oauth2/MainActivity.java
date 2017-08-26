7523a5f256392c2b3d6ec967d259fb342caef9c7
josdem <joseluis.delacruz@gmail.com>
2017-08-26 15:50:36 -0500

    Updating Google services

--- app/src/main/java/com/jos/dem/oauth2/MainActivity.java
+++ app/src/main/java/com/jos/dem/oauth2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
