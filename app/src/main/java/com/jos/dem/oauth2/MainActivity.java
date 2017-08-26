74a9a480237bcdbf4438030cef49781f8d34f18b
josdem <joseluis.delacruz@gmail.com>
2017-08-26 13:42:23 -0500

    Adding google services

--- app/src/main/java/com/jos/dem/oauth2/MainActivity.java
+++ app/src/main/java/com/jos/dem/oauth2/MainActivity.java
@@ -1,14 +1,41 @@
 package com.jos.dem.oauth2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+  private static final int RC_SIGN_IN = 9001;
+  private GoogleApiClient mGoogleApiClient;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build();
+
+    SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+    signInButton.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View view) {
+    signIn();
+  }
+
+  private void signIn() {
+    Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+    startActivityForResult(signInIntent, RC_SIGN_IN);
   }
 
 }
